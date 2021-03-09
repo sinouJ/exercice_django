06ed6a72495d8bd4b435c5d559cc920cbfa832b2
sinouJ <sinoujordan@gmail.com>
2021-03-09 23:25:12 +0100

    add data to template

--- site_rd/blog/views.py
+++ site_rd/blog/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from .models import Post
 
 def all_posts(request):
-    posts = Post.objects.filter(published_date)
-    return render(request, 'blog/all_posts.html')
+    posts = Post.objects.filter(is_published = "True").order_by('published_date')
+    return render(request, 'blog/all_posts.html', {'posts': posts})
