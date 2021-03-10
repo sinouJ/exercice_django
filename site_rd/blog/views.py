b814ae78f11347168a02c2c50c43157ec5d79c7e
sinouJ <sinoujordan@gmail.com>
2021-03-10 15:32:48 +0100

    add post view

--- site_rd/blog/views.py
+++ site_rd/blog/views.py
@@ -6,3 +6,7 @@ def all_posts(request):
     posts = Post.objects.filter(is_published = "True").order_by('published_date')
     users = User.objects.all()
     return render(request, 'blog/all_posts.html', {'posts': posts, 'users': users})
+
+def post_view(request, id):
+    post = Post.objects.get(id = id)
+    return render(request, 'blog/post_view.html', {'post': post})
