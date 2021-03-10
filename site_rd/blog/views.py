e9785cd7af253b89ab76ae8d0c767b88ec463df6
sinouJ <sinoujordan@gmail.com>
2021-03-10 14:33:35 +0100

    add author in view

--- site_rd/blog/views.py
+++ site_rd/blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from .models import Post
+from django.contrib.auth.models import User
 
 def all_posts(request):
     posts = Post.objects.filter(is_published = "True").order_by('published_date')
-    return render(request, 'blog/all_posts.html', {'posts': posts})
+    users = User.objects.all()
+    return render(request, 'blog/all_posts.html', {'posts': posts, 'users': users})
