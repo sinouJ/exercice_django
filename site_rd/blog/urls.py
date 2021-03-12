f52618a88f72ebab504a25fb1212b0ee0aee563f
sinouJ <sinoujordan@gmail.com>
2021-03-12 14:41:18 +0100

    add form create post

--- site_rd/blog/urls.py
+++ site_rd/blog/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.all_posts, name = 'all_posts'),
-    path('post/<int:id>/', views.post_view, name='post_view')
+    path('post/<int:id>/', views.post_view, name='post_view'),
+    path('post/new/', views.post_new, name='post_new')
 ]
--- site_rd/blog/views.py
+++ site_rd/blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from .models import Post
 from django.contrib.auth.models import User
+from .forms import PostForm
+from django.shortcuts import redirect
 
 def all_posts(request):
     posts = Post.objects.filter(is_published = "True").order_by('published_date')
@@ -10,3 +12,18 @@ def all_posts(request):
 def post_view(request, id):
     post = Post.objects.get(id = id)
     return render(request, 'blog/post_view.html', {'post': post})
+
+def post_new(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+
+        if form.is_valid():
+            post = form.save(commit = False)
+            post.author = request.user
+            post.is_published = False
+            post.save()
+            return redirect('post_view', id=post.id)
+
+    else:
+        form = PostForm()
+        return render(request, 'blog/post_new.html', {'form': form})
